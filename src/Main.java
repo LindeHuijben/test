235be71abf7973dd3841512ad4386c5e1afe35ec
Linde <la.huijbe@student.avans.nl>
2020-02-05 11:49:31 +0100

    rekenmachine

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world!");
         System.out.println("wat tekst");
+
+        System.out.println("rekenmachine");
+        System.out.println("3 + 6 = 9");
+
     }
 }
